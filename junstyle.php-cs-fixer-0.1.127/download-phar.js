79c563c12cc859843af7fee1d0b2530a91bf7f45
ahmarulabdi <rndmjck@gmai.com>
2019-11-05 11:21:33 +0700

    package for jhipster-angular

--- junstyle.php-cs-fixer-0.1.127/download-phar.js
+++
@@ -1,4 +0,0 @@
-let download = require('download');
-download('https://cs.sensiolabs.org/download/php-cs-fixer-v2.phar', __dirname, { 'filename': 'php-cs-fixer.phar' }).then(() => {
-	console.log('download php-cs-fixer.phar successfully.');
-});
--- sophisticode.php-formatter-0.2.4/out/filter.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-exports.DOCUMENT_FILTER = { language: 'php', scheme: 'file' };
-//# sourceMappingURL=filter.js.map
